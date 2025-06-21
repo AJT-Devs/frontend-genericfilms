b0a3279b66444d76167034870811c7e809cf0af7
tulio <tuliokenjiyoshimura@gmail.com>
2025-06-21 01:31:18 -0300

    change password and logout admin

--- adm/js/controller/perfil.js
+++ adm/js/controller/perfil.js
@@ -33,3 +33,61 @@ async function loadProfile(){
     profile.email.value = admin.email;
     
 }
+
+async function changePassword(){
+    const adminId = localStorage.getItem("admId");
+    const form = document.querySelector("#form-change-password");
+    const password = form.oldPassword.value;
+    const newPassword = form.newPassword.value;
+    
+    const data = {
+        password,
+        newPassword
+    }
+
+    console.log("Dados enviados: ", data);
+    const response = await fetch(`${urlServer}/admin/${+adminId}`, {
+        method: "PATCH",
+        headers: {
+            "Authorization" : `Bearer ${getToken()}`,
+            "Content-Type": "application/json"
+        },
+        credentials: 'include',
+        body: JSON.stringify(data)
+    });
+
+    if(response.status !== 200) {
+        const error = await response.json();
+        console.error("Erro: ", error);
+        alert(error.message);
+        return;
+    }
+    alert("Senha alterada com sucesso!");
+
+    window.location.href = "../../adm/screens/perfil.html";
+}
+
+async function logoutAdmin(){
+    const response = await fetch(`${urlServer}/admin/logout`, {
+        method: "DELETE",
+        headers: {
+            "Authorization" : `Bearer ${getToken()}`,
+            "Content-Type": "application/json"
+        },
+        credentials: 'include'
+    });
+
+    if(response.status !== 200) {
+        const error = await response.json();
+        console.error("Erro: ", error);
+        alert(error.message);
+        return;
+    }
+
+    alert("Logout realizado com sucesso!");
+    localStorage.removeItem("admId");
+    localStorage.removeItem("admToken");
+    localStorage.removeItem("admName");
+    window.location.href = "../../adm/";
+
+}
